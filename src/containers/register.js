2fc7b74590460b922227996070a2d0ddd72c833b
cruzach <charliecruzan@gmail.com>
2018-10-19 08:34:15 -0400

    cleanup

--- src/containers/register.js
+++ src/containers/register.js
@@ -21,7 +21,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignin = () => {
-        if(this.state.name.length === 0 || this.state.name.signInEmail.length === 0 || this.state.signInPassword.length === 0){
+        if(this.state.name.length === 0 || this.state.name.signInEmail === '' || this.state.signInPassword.length === 0){
             return alert('All input data is required.');
         }
         fetch('https://rocky-everglades-18419.herokuapp.com/register', {
@@ -56,28 +56,28 @@ class Register extends React.Component {
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Name</label>
                             <input 
-                            onChange={this.onNameChange} placeholder='Todd Packer' required
-                            className="pa2 input-reset ba bg-transparent hover-bg-black w-100" type="name" name="name"  id="name" />
+                                onChange={this.onNameChange} placeholder='Todd Packer' required
+                                className="pa2 input-reset ba bg-transparent hover-bg-black w-100" type="name" name="name"  id="name" />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input 
-                            onChange={this.onEmailChange} placeholder='packer@dm.com' required
-                            className="pa2 input-reset ba bg-transparent hover-bg-black w-100" type="email" name="email-address"  id="email-address" />
+                                onChange={this.onEmailChange} placeholder='packer@dm.com' required
+                                className="pa2 input-reset ba bg-transparent hover-bg-black w-100" type="email" name="email-address"  id="email-address" />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                             <input 
-                            onChange={this.onPasswordChange}
-                            className="b pa2 input-reset ba bg-transparent hover-bg-black w-100" type="password" name="password"  id="password" />
+                                onChange={this.onPasswordChange} required
+                                className="b pa2 input-reset ba bg-transparent hover-bg-black w-100" type="password" name="password"  id="password" />
                         </div>
                         </fieldset>
                         <div className="">
                             <input 
-                            type="submit" 
-                            onClick={this.onSubmitSignin}
-                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                            value="Register" />
+                                type="submit" 
+                                onClick={this.onSubmitSignin}
+                                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                                value="Register" />
                         </div>
                         
                     </div>
